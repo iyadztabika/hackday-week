da331d2f6228b43b564b66fc7b8c149f8b57e4c8
Indana Rishi <iyadztabika@refactory.id>
2022-03-13 09:12:12 +0700

    fetch and display list of posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+// styles
 import './App.css';
 
 function App() {
+  const [listOfPosts, setListOfPosts] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/posts')
+      .then((res) => {
+        setListOfPosts(res.data)
+      })
+  }, [])
+
   return (
     <div className="App">
-      hello world
+      {listOfPosts.map((post, index) => (
+        <div className='post' key={index}>
+          <div className="title">{post.title}</div>
+          <div className="body">{post.postText}</div>
+          <div className="footer">{post.username}</div>
+        </div>
+      ))}
     </div>
   );
 }
