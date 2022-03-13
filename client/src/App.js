63af102cfd29b9c21919bcfb3ddcfc6bb1a5a918
Indana Rishi <iyadztabika@refactory.id>
2022-03-13 13:24:56 +0700

    add styles for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,10 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Link to='/createpost'>Create a Post</Link>
-        <Link to='/'>Home Page</Link>
+        <div className="navbar">
+          <Link to='/'>Home Page</Link>
+          <Link to='/createpost'>Create a Post</Link>
+        </div>
         <Switch>
           <Route path='/' component={Home} exact />
           <Route path='/createpost' component={CreatePost} />
