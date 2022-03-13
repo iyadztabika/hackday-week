1c52185211137546eb5d44fc8b120258b580591b
Indana Rishi <iyadztabika@refactory.id>
2022-03-13 10:13:50 +0700

    create home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,15 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+// pages
+import Home from './pages/Home';
 
 // styles
 import './App.css';
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([])
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:5000/posts')
-      .then((res) => {
-        setListOfPosts(res.data)
-      })
-  }, [])
+  
 
   return (
     <div className="App">
-      {listOfPosts.map((post, index) => (
-        <div className='post' key={index}>
-          <div className="title">{post.title}</div>
-          <div className="content">{post.postText}</div>
-          <div className="footer">{post.username}</div>
-        </div>
-      ))}
+      <Home />
     </div>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Home = () => {
+
+    const [listOfPosts, setListOfPosts] = useState([])
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:5000/posts')
+        .then((res) => {
+            setListOfPosts(res.data)
+        })
+    }, [])
+
+    return (
+        <div>
+            {listOfPosts.map((post, index) => (
+                <div className='post' key={index}>
+                <div className="title">{post.title}</div>
+                <div className="content">{post.postText}</div>
+                <div className="footer">{post.username}</div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Home
