e31ce055ef153682d688dc9b207b7dfbc9793cbc
Indana Rishi <iyadztabika@refactory.id>
2022-03-13 14:13:09 +0700

    fetch and display single post data

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,16 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 const Post = () => {
+    const [postData, setPostData] = useState({})
     const { id } = useParams()
 
     useEffect(() => {
-        
-    }, [])
+        axios
+            .get(`http://localhost:5000/posts/single/${id}`)
+            .then(res => {
+                setPostData(res.data)
+            })
+            .catch(err => {
+                console.log(err.response.status)
+            })
+    }, [id])
 
     return (
-        <div>Post</div>
+        <div>
+            {postData.username}
+        </div>
     )
 }
 
